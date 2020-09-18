01b45dde333613037ae0e8fdad2ffa7de2df7989
Frostack <soulburn007@gmail.com>
2020-09-18 22:05:54 +0430

    🌐 change font to IranSans

--- src/common/styled.js
+++ src/common/styled.js
@@ -1,6 +1,6 @@
 import styled, { createGlobalStyle } from 'styled-components/macro'
 
-import font from '../assets/fonts/Yekan.ttf'
+import font from '../assets/fonts/Iranian-Sans.ttf'
 
 export const GlobalStyles = createGlobalStyle`
  html {
@@ -11,29 +11,25 @@ export const GlobalStyles = createGlobalStyle`
  *::before,
  *::after {
     box-sizing: inherit;
-    transition: 0.2s;
   }
 
   @font-face {
-    font-family: 'Yekan';
-    src: local('Yekan'), url(${font}) format('truetype');
+    font-family: 'IranSans';
+    src: local('IranSans'), url(${font}) format('truetype');
   }
 
   body {
     direction: rtl;
-    font-family: Yekan;
+    font-family: IranSans;
     background-color: #ECEFF1;
     color: #263238;
   }
 `
 
-export const IconButton = styled.button`
+export const IconButton = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 0.5rem;
   cursor: pointer;
-  background-color: inherit;
-  border: none;
-  color: inherit;
 `

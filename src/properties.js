e1b995fe91bc6ae440b1f87d9f5a9e3edf83d64e
Frostack <soulburn007@gmail.com>
2020-09-18 18:10:54 +0430

    🌐 add properties file

---
+++ src/properties.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const properties = {
+  header: {
+    logo: 'Itemist',
+  },
+  pages: {
+    home: {
+      heading: (
+        <>
+          راحت ترین راه برای
+          <br /> ترید آیتم‌های
+          <br /> استیم
+        </>
+      ),
+      callToAction: 'همین حالا شروع کن',
+    },
+  },
+}
+
+export default properties

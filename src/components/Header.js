b482e8ed34669c4c3a7c1cb07c9b198748616ca4
Frostack <soulburn007@gmail.com>
2020-09-18 18:27:15 +0430

    💄 fix header padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,20 @@ const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   font-size: 1.5em;
-  padding: 1rem 1.5rem;
+  padding: 0.75rem 1rem;
 `
 
 const Logo = styled.div`
   font-weight: bold;
   font-family: 'Lobster', cursive;
+  padding: 0.5rem;
 `
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 0.5rem;
 `
 
 function Header({ logo }) {

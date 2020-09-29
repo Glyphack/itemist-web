c32a84ec6e925667700fcf043f7e87fbaf6949c8
glyphack <sh.hooshyari@gmail.com>
2020-09-29 21:37:10 +0330

    use inventory component in inventory page

--- src/screens/Inventory.js
+++ src/screens/Inventory.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
+import InventoryComponent from '../components/Inventory'
+
 function Inventory() {
-  return <div>Inventory Page</div>
+  return (
+    <div>Inventory Page
+      <InventoryComponent/>
+    </div>
+  )
 }
 
 export default Inventory

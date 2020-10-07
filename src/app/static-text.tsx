1de6a1600541364e58e475a808a2fcfcd3b2cb03
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-07 13:44:55 +0330

    🌐 add static text

---
+++ src/app/static-text.tsx
@@ -0,0 +1,17 @@
+export const staticText = {
+  layout: {
+    logo: 'آیتمیست',
+    drawer: {
+      login: 'ورود با استیم',
+      shop: 'فروشگاه',
+    },
+  },
+  home: {
+    heading: 'راحت ترین راه برای ترید، خرید و فروش آیتم های استیم',
+    callToAction: 'همین حالا شروع کن',
+  },
+  sell: 'فروش',
+  putItUpForSale: 'ثبت برای فروش',
+  tradeOfferSentMessage: 'ترید آفر برای شما ارسال شد',
+  steamCdnUrl: 'https://community.cloudflare.steamstatic.com/economy/image/',
+}

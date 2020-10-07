384748043d708c0171d1dcbe7388c35bab738a60
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-07 20:33:37 +0330

    🚚 change extension to .ts

--- src/app/static-text.ts
+++ src/app/static-text.ts
@@ -10,6 +10,16 @@ export const staticText = {
     heading: 'راحت ترین راه برای ترید، خرید و فروش آیتم های استیم',
     callToAction: 'همین حالا شروع کن',
   },
+  shop: {
+    search: 'جستجو...',
+    filter: {
+      button: 'فیلتر ها',
+      nameAsc: 'به ترتیب نام: صعودی',
+      nameDesc: 'به ترتیب نام: نزولی',
+      priceAsc: 'قیمت: صعودی',
+      priceDesc: 'قیمت: نزولی',
+    },
+  },
   sell: 'فروش',
   putItUpForSale: 'ثبت برای فروش',
   tradeOfferSentMessage: 'ترید آفر برای شما ارسال شد',

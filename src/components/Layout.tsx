6b542fc0e4cdd6f93d9d58282f9eb69bb3a69678
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-07 20:34:16 +0330

    🧱 change Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { Box } from '@chakra-ui/core'
+
 import { Header } from './Header'
 
-export function Layout() {
-  return <Header />
+type LayoutProps = {
+  children: JSX.Element
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <Box color="gray.900">
+      <Header />
+      {children}
+    </Box>
+  )
 }

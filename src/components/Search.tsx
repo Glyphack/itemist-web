1c5647ca46df09303cf94322e64d1c7a4c8576ae
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-07 20:36:29 +0330

    🧱 add Search component

---
+++ src/components/Search.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { InputGroup, InputLeftElement, Input, Icon } from '@chakra-ui/core'
+import { HiSearch } from 'react-icons/hi'
+
+import { staticText } from '../app/static-text'
+
+export function Search() {
+  return (
+    <InputGroup ml={2}>
+      <InputLeftElement
+        pointerEvents="none"
+        children={<Icon as={HiSearch} boxSize={5} color="gray.500" />}
+      />
+      <Input variant="filled" placeholder={staticText.shop.search} />
+    </InputGroup>
+  )
+}

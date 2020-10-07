a9debcacf44f7a15f7b270ab13c20162d3799a03
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-07 13:48:00 +0330

    ♻ refactor code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
-import * as React from "react"
-import ReactDOM from "react-dom"
-import { App } from "./App"
-import * as serviceWorker from "./serviceWorker"
-import reportWebVitals from "./reportWebVitals"
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import * as serviceWorker from './serviceWorker'
+import { App } from './app/App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById('root')
 )
 
 // If you want your app to work offline and load faster, you can change

c976086283cdaaaf41cf94a2eb2e3dbd491a2b01
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-08 16:00:29 +0330

    ♻ seperate navgiation drawer and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,31 @@
-import React from 'react'
-import { Flex, IconButton, useDisclosure, Heading, Avatar } from '@chakra-ui/core'
+import React, { RefObject } from 'react'
+import { Link as RouteLink } from 'react-router-dom'
+import { Flex, IconButton, Avatar, Link, Icon } from '@chakra-ui/core'
 import { HiMenu } from 'react-icons/hi'
 
 import { staticText } from '../app/static-text'
-import { NavigationDrawer } from './NavigationDrawer'
 
-export function Header() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = React.useRef<HTMLButtonElement>(null!)
+type HeaderProps = {
+  drawerBtnRef: RefObject<HTMLButtonElement>
+  onDrawerOpen: () => void
+}
 
+export function Header({ drawerBtnRef, onDrawerOpen }: HeaderProps) {
   return (
-    <>
-      <Flex justifyContent="space-between" alignItems="center" p={3}>
-        <Heading size="lg">{staticText.layout.logo}</Heading>
-        <Flex alignItems="center">
-          <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" size="sm" mx={4} />
-          <IconButton
-            aria-label="open navigation drawer"
-            ref={btnRef}
-            onClick={onOpen}
-            variant="ghost"
-            icon={<HiMenu size={32} />}
-          />
-        </Flex>
+    <Flex justifyContent="space-between" alignItems="center" p={3}>
+      <Link fontSize="2xl" fontWeight="bold" size="lg" as={RouteLink} to="/">
+        {staticText.layout.logo}
+      </Link>
+      <Flex alignItems="center">
+        <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" size="sm" mx={4} />
+        <IconButton
+          aria-label="open navigation drawer"
+          ref={drawerBtnRef}
+          onClick={onDrawerOpen}
+          variant="ghost"
+          icon={<Icon as={HiMenu} boxSize="32px" />}
+        />
       </Flex>
-      <NavigationDrawer isOpen={isOpen} onClose={onClose} finalFocusRef={btnRef} />
-    </>
+    </Flex>
   )
 }

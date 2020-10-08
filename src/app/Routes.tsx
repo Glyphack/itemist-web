445e9ef413d575eeca6ec2d52572ddee41aa8edb
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-08 23:21:03 +0330

    🛣  add new routes

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -5,18 +5,24 @@ import { Layout } from './Layout'
 import { NotFound } from '../screens/NotFound'
 import { Home } from '../screens/Home'
 import { InventoryShop } from '../screens/InventoryShop'
+import { ShoppingCart } from '../screens/ShoppingCart'
 
 export function Routes() {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/">
+            <Home />
+          </Route>
           <Route exact path="/shop">
             <InventoryShop variant="shop" />
           </Route>
           <Route exact path="/inventory">
             <InventoryShop variant="inventory" />
+          </Route>
+          <Route exact path="/shopping-cart">
+            <ShoppingCart />
           </Route>
           <Route component={NotFound} />
         </Switch>

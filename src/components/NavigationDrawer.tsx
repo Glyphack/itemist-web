b73f6fcf0d62819c3724b10286b9dc2048b38f1a
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-09 17:38:21 +0330

    ✨ use steam auth

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -13,6 +13,7 @@ import {
 } from '@chakra-ui/core'
 
 import { staticText } from '../app/static-text'
+import { useSteamAuth } from '../hooks/use-steam-auth'
 import { EditableField } from './EditableField/EditableField'
 
 type NavigationDrawerProps = {
@@ -22,44 +23,45 @@ type NavigationDrawerProps = {
 }
 
 export function NavigationDrawer({ isOpen, onClose, finalFocusRef }: NavigationDrawerProps) {
+  const handleLogin = useSteamAuth()
+
   return (
     <Drawer
       isOpen={isOpen}
       onClose={onClose}
       finalFocusRef={finalFocusRef}
       placement="left"
-      size="full"
+      size="sm"
     >
       <DrawerOverlay>
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerBody>
-            <VStack
-              spacing={6}
-              fontSize="3xl"
-              textAlign="center"
-              justify="center"
-              alignItems="center"
-              mt={12}
-            >
-              <VStack mb={4} fontSize="lg" color="gray.500">
-                <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" size="2xl" />
-                <Text bg="gray.50">Dan Abrahmov</Text>
-                <EditableField />
-              </VStack>
-              <Link as={RouteLink} to="/" onClick={onClose}>
-                {staticText.layout.drawer.login}
-              </Link>
-              <Link as={RouteLink} to="/shop" onClick={onClose}>
-                {staticText.layout.drawer.shop}
-              </Link>
-              <Link as={RouteLink} to="/inventory" onClick={onClose}>
-                {staticText.layout.drawer.inventory}
-              </Link>
-              <Link as={RouteLink} to="/" onClick={onClose} color="red.500">
-                {staticText.layout.drawer.logout}
-              </Link>
+          <DrawerBody
+            as={VStack}
+            spacing={6}
+            fontSize="3xl"
+            textAlign="center"
+            justify="center"
+            alignItems="center"
+          >
+            <VStack mb={4} fontSize="lg" color="gray.500">
+              <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" size="2xl" />
+              <Text bg="gray.50">Dan Abrahmov</Text>
+              <EditableField />
             </VStack>
+
+            <Link as={RouteLink} to="/" onClick={handleLogin}>
+              {staticText.layout.drawer.login}
+            </Link>
+            <Link as={RouteLink} to="/shop" onClick={onClose}>
+              {staticText.layout.drawer.shop}
+            </Link>
+            <Link as={RouteLink} to="/inventory" onClick={onClose}>
+              {staticText.layout.drawer.inventory}
+            </Link>
+            <Link as={RouteLink} to="/" onClick={onClose} color="red.500">
+              {staticText.layout.drawer.logout}
+            </Link>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>

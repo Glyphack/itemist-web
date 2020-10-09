891fe2be899c03a864191d77835fc991c60d0394
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-09 17:34:00 +0330

    ⚙ add axios config

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Authorization: `Bearer ${Cookies.get('access_token')}`,
+  },
+})
+
+declare namespace EndpointTypes {
+  type get = '/profile' | '/profile/inventory' | '/sell' | '/products' | '/cart' | '/cart/checkout'
+  type post = '/sell'
+  type put = '/profile'
+}
+
+export const api = {
+  get: (endpoint: EndpointTypes.get) => instance.get(endpoint),
+  post: (endpoint: EndpointTypes.post) => instance.post(endpoint),
+  put: (endpoint: EndpointTypes.put) => instance.put(endpoint),
+}

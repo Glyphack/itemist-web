d623c79b9bffa701bff059aba06a147f15165773
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-11 17:45:27 +0330

    add inventory item amount

--- src/components/Items/InventoryItem.tsx
+++ src/components/Items/InventoryItem.tsx
@@ -3,14 +3,18 @@ import { Text, Button, useDisclosure } from '@chakra-ui/core'
 
 import { SellModal } from '../SellModal'
 
-export function InventoryItem() {
+type InventoryItemProps = {
+  amount: string
+}
+
+export function InventoryItem({ amount }: InventoryItemProps) {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const cancelRef = useRef(null!)
 
   return (
     <>
       <Text as="strong" color="gray.600" mr={2}>
-        10x
+        {amount}
       </Text>
       <Button colorScheme="green" w="100%" ref={cancelRef} onClick={onOpen}>
         فروش

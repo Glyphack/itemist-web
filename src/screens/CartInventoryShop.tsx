eba77bfba267125d7b29094e6a20748518938875
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-11 16:16:33 +0330

    🦴 items screen now render skeleton if data is not available

--- src/screens/CartInventoryShop.tsx
+++ src/screens/CartInventoryShop.tsx
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from 'react'
-import { Flex } from '@chakra-ui/core'
+import { Flex, Skeleton } from '@chakra-ui/core'
 
 import { Search } from '../components/Search'
 import { FilterSelection } from '../components/FilterSelection'
 import { ItemList } from '../components/Items/ItemList'
 import { ItemProps } from '../components/Items/Item'
 import { api } from '../api'
+import { Schemas } from '../api/schemas'
+import { ItemsSkeleton } from '../components/Items/ItemsSkeleton'
 
 export function CartInventoryShop({ variant }: ItemProps) {
-  const [items, setItems] = useState(null)
+  const [items, setItems] = useState<Schemas.InventoryItem[] | null>(null)
 
   useEffect(() => {
     switch (variant) {
       case 'inventory':
-        api.get('/profile/inventory').then(response => setItems(response.data))
+        api.get('/profile/inventory').then(response => setItems(response.data.inventory))
         break
       case 'shop':
-        api.get('/products').then(response => setItems(response.data))
+        api.get('/products').then(response => setItems(response.data.inventory))
         break
       case 'shopping-cart':
-        api.get('/cart').then(response => setItems(response.data))
+        api.get('/cart').then(response => setItems(response.data.inventory))
     }
   }, [variant])
+
+  if (!items) {
+    return (
+      <>
+        {variant !== 'shopping-cart' ? <ListFilters /> : null}
+        <ItemsSkeleton variant={variant} />
+      </>
+    )
+  }
 
   return (
     <>
       {variant !== 'shopping-cart' ? <ListFilters /> : null}
-      <ItemList variant={variant} />
+      <ItemList variant={variant} items={items} />
     </>
   )
 }

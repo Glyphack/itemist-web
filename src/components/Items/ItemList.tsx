dc27b722c472c846169900fe57b8c5e32c2e9c20
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-11 17:45:47 +0330

    pass data to items

--- src/components/Items/ItemList.tsx
+++ src/components/Items/ItemList.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { SimpleGrid } from '@chakra-ui/core'
 
-import { Item, ItemProps } from './Item'
+import { Item, ItemVariant } from './Item'
 import { Schemas } from '../../api/schemas'
 
-type ItemListProps = ItemProps & {
+type ItemListProps = {
   items: Schemas.InventoryItem[]
+  variant: ItemVariant
 }
 
 export function ItemList({ items, variant }: ItemListProps) {
   return (
     <SimpleGrid mt={6} spacing={10} minChildWidth="288px">
-      {items.map((_, i) => (
-        <Item key={i} variant={variant} />
+      {items.map(item => (
+        <Item key={item.pos} variant={variant} data={item} />
       ))}
     </SimpleGrid>
   )

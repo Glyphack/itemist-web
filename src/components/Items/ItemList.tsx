b208071b33f1971845145437d314f8fcc4a57e47
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-11 16:15:37 +0330

    🏷 item list now accepts items

--- src/components/Items/ItemList.tsx
+++ src/components/Items/ItemList.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { SimpleGrid } from '@chakra-ui/core'
 
 import { Item, ItemProps } from './Item'
+import { Schemas } from '../../api/schemas'
 
-export function ItemList({ variant }: ItemProps) {
+type ItemListProps = ItemProps & {
+  items: Schemas.InventoryItem[]
+}
+
+export function ItemList({ items, variant }: ItemListProps) {
   return (
     <SimpleGrid mt={6} spacing={10} minChildWidth="288px">
-      {[...new Array(20)].map((_, i) => (
+      {items.map((_, i) => (
         <Item key={i} variant={variant} />
       ))}
     </SimpleGrid>

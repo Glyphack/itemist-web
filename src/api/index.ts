e3ca059cbee5e7628905b17998e11bd72dc033f3
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-15 23:37:23 +0330

    add new endpoints

--- src/api/index.ts
+++ src/api/index.ts
@@ -17,15 +17,18 @@ instance.interceptors.request.use(
 
 declare namespace EndpointTypes {
   type get = '/profile' | '/profile/inventory' | '/sell' | '/products' | '/cart' | '/cart/checkout'
-  type post = '/sell'
+  type post = '/sell' | '/cart'
   type put = '/profile'
+}
+
+declare namespace Requests {
+  type sellPostData = { appId: string; contextId: string; assetId: string; price: number }
+  type cartPostData = { productId: string }
 }
 
 export const api = {
   get: (endpoint: EndpointTypes.get) => instance.get(endpoint),
-  post: (
-    endpoint: EndpointTypes.post,
-    sellOrder: { appId: string; contextId: string; assetId: string; price: number }
-  ) => instance.post(endpoint, sellOrder),
+  post: (endpoint: EndpointTypes.post, sellOrder: Requests.cartPostData | Requests.sellPostData) =>
+    instance.post(endpoint, sellOrder),
   put: (endpoint: EndpointTypes.put, tradeUrl: string) => instance.put(endpoint, { tradeUrl }),
 }

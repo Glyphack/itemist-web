8db5ca0f98f36750fa1d524d53c759287fb3c645
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-15 23:39:06 +0330

    integrate cart api

--- src/components/Items/ShopItem.tsx
+++ src/components/Items/ShopItem.tsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSetRecoilState } from 'recoil'
 import { Flex, Text, Button } from '@chakra-ui/core'
 
 import { Schemas } from '../../api/schemas'
+import { cartState } from '../../recoil/cart-state'
+import { api } from '../../api'
 
 type ShopItemProps = {
   data: Schemas.Product
 }
 
 export function ShopItem({ data }: ShopItemProps) {
+  const setCartState = useSetRecoilState(cartState)
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  const handleClick = () => {
+    setIsDisabled(true)
+    setCartState(({ count }) => ({ count: count + 1 }))
+    api.post('/cart', { productId: data._id })
+  }
+
   return (
     <>
       <Flex>
@@ -24,7 +36,7 @@ export function ShopItem({ data }: ShopItemProps) {
           </Text>
         </Text> */}
       </Flex>
-      <Button colorScheme="blue" w="100%">
+      <Button colorScheme="blue" w="100%" disabled={isDisabled} onClick={handleClick}>
         خرید
       </Button>
     </>

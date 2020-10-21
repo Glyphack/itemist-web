f99abe21151cf4cde4ebe2499dd857f9b6129a32
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-21 15:58:30 +0330

    add vertical stacks

--- src/screens/Shop.tsx
+++ src/screens/Shop.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import { VStack } from '@chakra-ui/core'
+
 import { api } from '../api'
 import { Schemas } from '../api/schemas'
-import { ItemList } from '../components/Items/ItemList'
-
 import { ListFilters } from '../components/ListFilters'
+import { ItemList } from '../components/Items/ItemList'
 
 export function Shop() {
   const [items, setItems] = useState<Schemas.Product[] | null>(null)
@@ -14,9 +15,9 @@ export function Shop() {
   }, [])
 
   return (
-    <>
+    <VStack spacing={8} align="stretch">
       <ListFilters />
       <ItemList variant="shop" items={items} />
-    </>
+    </VStack>
   )
 }

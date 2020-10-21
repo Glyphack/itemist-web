332842677aed120888d6092c9ea78e7a2d95b9b9
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-21 16:54:07 +0330

    fix spacing

--- src/components/Items/ItemList.tsx
+++ src/components/Items/ItemList.tsx
@@ -14,7 +14,7 @@ export function ItemList({ items, variant }: ItemListProps) {
   if (!items) return <ItemsSkeleton variant={variant} />
 
   return (
-    <SimpleGrid spacing={8} minChildWidth="288px">
+    <SimpleGrid spacing={10} minChildWidth="288px">
       {items.map(item => {
         if ('price' in item) {
           return (

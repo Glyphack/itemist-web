c3549dc7d2b1f52f5d3151d733fd3948609e0977
Erfan QasemiZade <soulburn007@gmail.com>
2020-10-27 13:45:18 +0330

    add loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useRecoilValue } from 'recoil'
+import { Center, CircularProgress, Flex } from '@chakra-ui/core'
+
+import { loadingState } from '../recoil/loading-state'
+
+export function Loading() {
+  const { isLoading } = useRecoilValue(loadingState)
+
+  if (!isLoading) return null
+
+  return (
+    <Center w="100vw" h="100vh" bg="#000000aa" pos="fixed" top="0" left="0">
+      <CircularProgress isIndeterminate />
+    </Center>
+  )
+}

2876f03d6a3ff61085412a8f38114a353da2841f
frustak <soulburn007@gmail.com>
2021-03-19 17:41:29 +0330

    improve payment response views

--- src/screens/Payment.tsx
+++ src/screens/Payment.tsx
@@ -1,28 +1,36 @@
-import { Box, Heading } from '@chakra-ui/layout'
+import { Box, Heading, Text } from '@chakra-ui/layout'
 import React, { useEffect, useState } from 'react'
 import { staticText } from '../app/static-text'
 
 export namespace Payment {
-  export function Success() {
-    const [refId, setRefId] = useState<string | null>(null)
+  const useOrderId = () => {
+    const [orderId, setOrderId] = useState<string | null>(null)
 
     useEffect(() => {
       const searchParams = new URLSearchParams(window.location.search)
-      setRefId(searchParams.get('refId'))
-    }, [refId])
+      setOrderId(searchParams.get('orderId'))
+    }, [orderId])
+
+    return orderId
+  }
+
+  export function Success() {
+    const orderId = useOrderId()
 
     return (
       <Box bgColor="green.500" px={3} py={4} borderRadius="base">
         <Heading as="h2" size="md" lineHeight="1.5">
           {staticText.payment.success.msg}
           <br />
-          {staticText.payment.success.number} {refId}
+          {staticText.payment.success.number} {orderId}
         </Heading>
       </Box>
     )
   }
 
   export function Failed() {
+    const orderId = useOrderId()
+
     return (
       <Box bgColor="red.500" px={3} py={4} borderRadius="base">
         <Heading as="h2" size="md" lineHeight="1.5">
@@ -30,6 +38,10 @@ export namespace Payment {
           <br />
           {staticText.payment.error.phone}
         </Heading>
+        <Text>
+          {staticText.payment.error.orderId}
+          {orderId}
+        </Text>
       </Box>
     )
   }

a19edcbbc1d998d3f88962519e3bda7217179636
frustak <soulburn007@gmail.com>
2021-06-19 18:17:12 +0430

    use new search functionality

--- src/screens/Shop.tsx
+++ src/screens/Shop.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { VStack } from '@chakra-ui/react'
-
 import { api } from '../api'
-import { Schemas } from '../api/schemas'
 import { ListFilters } from '../components/ListFilters'
 import { ItemList } from '../components/Items/ItemList'
+import { useRecoilState } from 'recoil'
+import { itemsState } from '../recoil/items-state'
 
 export function Shop() {
-  const [items, setItems] = useState<Schemas.Product[] | null>(null)
+  const [items, setItems] = useRecoilState(itemsState)
 
   useEffect(() => {
     setItems(null)
     api.get('/products').then(response => setItems(response.data))
-  }, [])
+  }, [setItems])
 
   return (
     <VStack spacing={8} align="stretch">
-      <ListFilters />
+      <ListFilters variant="shop" />
       <ItemList variant="shop" items={items} />
     </VStack>
   )

b2e28a081f8c4ebe3369b9663f0d4bb53015a701
frustak <soulburn007@gmail.com>
2021-06-19 18:16:53 +0430

    add search functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import { InputGroup, InputLeftElement, Input, Icon } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { InputGroup, InputLeftElement, Input, Icon, Box } from '@chakra-ui/react'
 import { HiSearch } from 'react-icons/hi'
-
 import { staticText } from '../app/static-text'
+import { useSetRecoilState } from 'recoil'
+import { itemsState } from '../recoil/items-state'
+import { api, EndpointTypes } from '../api'
+import { ItemVariant } from './Items/Item'
 
-export function Search() {
+type SearchProps = {
+  variant: ItemVariant
+}
+
+export function Search({ variant }: SearchProps) {
+  const [searchValue, setSearchValue] = useState('')
+  const setItems = useSetRecoilState(itemsState)
+
+  const handleSubmit = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+
+    let route: EndpointTypes.Get
+    switch (variant) {
+      case 'inventory':
+        route = '/profile/inventory'
+        break
+      case 'shop':
+        route = '/products'
+        break
+      default:
+        throw new Error('Unknown variant!')
+    }
+
+    setItems(null)
+    const response = await api.get(route, {
+      params: { name: searchValue },
+    })
+    setItems(response.data)
+  }
+
   return (
-    <InputGroup ml={2}>
-      <InputLeftElement
-        pointerEvents="none"
-        children={<Icon as={HiSearch} boxSize={5} color="gray.500" />}
-      />
-      <Input variant="filled" placeholder={staticText.shop.search} fontFamily="Arial" />
-    </InputGroup>
+    <Box as="form" onSubmit={handleSubmit} w="100%">
+      <InputGroup ml={2}>
+        <InputLeftElement
+          pointerEvents="none"
+          children={<Icon as={HiSearch} boxSize={5} color="gray.500" />}
+        />
+        <Input
+          variant="filled"
+          placeholder={staticText.shop.search}
+          fontFamily="Arial"
+          onChange={v => setSearchValue(v.target.value)}
+        />
+      </InputGroup>
+    </Box>
   )
 }
